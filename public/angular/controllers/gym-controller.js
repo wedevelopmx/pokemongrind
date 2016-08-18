2aa6a6aa27714e93593113c0b17044174d5c89ff
Cristian Colorado <ccoloradoc@gmail.com>
2016-08-18 12:53:23 -0500

    BugFix: FBComments dynamic href reference

--- public/angular/controllers/gym-controller.js
+++ public/angular/controllers/gym-controller.js
@@ -1,6 +1,8 @@
 angular.module('app')
   .controller('GymController', ['$scope', '$routeParams', 'GymService', function($scope, $routeParams, GymService) {
-    
+
+    $scope.eventUrl = 'http://pokemongrind.com/#/gym/' + $routeParams.id;
+
     GymService.get({ gymId: $routeParams.id }, function(gym) {
       $scope.gym = gym;
     });
